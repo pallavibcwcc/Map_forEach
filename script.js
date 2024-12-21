ac78be9eb19047433a27b52d45dc691699965b2e
pallavibcwcc <pallavi.bcwcc@gmail.com>
2024-12-21 11:31:04 +0530

    forEach

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const Arr = [1,2,3,4];
- const Double = Arr.map((ele)=>ele*2);
- console.log(Double);
+//  const Double = Arr.map((ele)=>ele*2);
+//  console.log(Double);
+
+ const triple = Arr.forEach((val,i,a)=>(a[i]=val*3));
+ console.log(triple,Arr);
